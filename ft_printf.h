bd9afeecad5042c3f50454a12345330aab53a8cb
Sawyerf <sawyer.flink@protonmail.ch>
2018-11-27 14:49:51 +0100

    Add: .h

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeyret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/27 14:31:37 by apeyret           #+#    #+#             */
+/*   Updated: 2018/11/27 14:36:31 by apeyret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+
+# include <unistd.h>
+# include "libft.h"
+
+typedef struct	s_printf
+{
+	char		*raw;
+	char		type;
+	int			pre;
+	char		*flags;
+	struct s_printf	*next;
+}				t_printf;
+
+#endif
