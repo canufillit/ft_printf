1d8c515afda30f3172c4977e54e3b0e717373bf9
Sawyerf <sawyer.flink@protonmail.ch>
2018-12-03 18:53:33 +0100

    Fix: cast short

--- pf_router.c
+++ pf_router.c
@@ -6,7 +6,7 @@
 /*   By: apeyret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 13:37:11 by apeyret           #+#    #+#             */
-/*   Updated: 2018/12/03 18:46:10 by apeyret          ###   ########.fr       */
+/*   Updated: 2018/12/03 18:52:31 by apeyret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ void	pf_router_d(t_printf *lst, va_list ap)
 	else if (lst->size[0] == 'l')
 		lst->var = stoa_base(lst, va_arg(ap, long), pf_base(lst->type));
 	else if (lst->size[1] == 'h')
-		lst->var = stoa_base(lst, va_arg(ap, int), pf_base(lst->type));
+		lst->var = stoa_base(lst, (char)va_arg(ap, int), pf_base(lst->type));
 	else if (lst->size[0] == 'h')
-		lst->var = stoa_base(lst, va_arg(ap, int), pf_base(lst->type));
+		lst->var = stoa_base(lst, (short)va_arg(ap, int), pf_base(lst->type));
 	lst->var = pf_options(lst);
 	ft_putstr(lst->var);
 }
@@ -51,9 +51,9 @@ void	pf_router_u(t_printf *lst, va_list ap)
 	else if (lst->size[0] == 'l')
 		lst->var = utoa_base(lst, va_arg(ap, unsigned long), pf_base(lst->type));
 	else if (lst->size[1] == 'h')
-		lst->var = utoa_base(lst, va_arg(ap, unsigned int), pf_base(lst->type));
+		lst->var = utoa_base(lst, (unsigned char)va_arg(ap, unsigned int), pf_base(lst->type));
 	else if (lst->size[0] == 'h')
-		lst->var = utoa_base(lst, va_arg(ap, unsigned int), pf_base(lst->type));
+		lst->var = utoa_base(lst, (unsigned short)va_arg(ap, unsigned int), pf_base(lst->type));
 	lst->var = pf_options(lst);
 	if (lst->type == 'x')
 		lst->var = ft_strlower(lst->var);
